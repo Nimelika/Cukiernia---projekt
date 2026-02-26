f35d481831a8068199ee58af75e15946ea5e9149
Nimelika <darkphoenix610@gmail.com>
2026-02-27 00:44:45 +0100

    Add files via upload

---
+++ MultipleObjectsViews/MultipleObjectsDictionaries/AllCelebrationCakesView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopApp.Views.MulitipleObjectsViews.MulitipleObjectsDictionaries
+{
+    public partial class AllCelebrationCakesView : AllViewBase
+    {
+        public AllCelebrationCakesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
