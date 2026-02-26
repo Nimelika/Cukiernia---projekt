bd3cb43303acc77c5417ce0faf9c58be9c24cb7a
Nimelika <darkphoenix610@gmail.com>
2026-02-27 00:45:28 +0100

    Add files via upload

---
+++ SingleObjectViews/SingleObjectDictionaries/CreateCakeFillingView.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp.Views.SingleObjectViews
+{
+	/// <summary>
+	/// Interaction logic for CreateCakeFillingView.xaml
+	/// </summary>
+	public partial class CreateCakeFillingView : SingleViewBase
+	{
+		public CreateCakeFillingView()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
+
+
---
+++ SingleObjectViews/SingleObjectDictionaries/OrderItem/CreateOrderItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp.Views.SingleObjectViews.SingleObjectDictionaries.OrderItem
+{
+    /// <summary>
+    /// Interaction logic for CreateOrderItemView.xaml
+    /// </summary>
+    public partial class CreateOrderItemView : SingleViewBase
+    {
+        public CreateOrderItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
