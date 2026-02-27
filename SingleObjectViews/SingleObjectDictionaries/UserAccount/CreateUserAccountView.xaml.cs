d827acf35ff8133da7c789bcc397c8e96694e6eb
Nimelika <darkphoenix610@gmail.com>
2026-02-28 00:27:51 +0100

    Add files via upload

---
+++ SingleObjectViews/SingleObjectDictionaries/UserAccount/CreateUserAccountView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DesktopApp.ViewModels.SingleObjectViewModels.SingleObjectDictionariesVM.UserAccountVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp.Views.SingleObjectViews.SingleObjectDictionaries.UserAccount
+{
+    /// <summary>
+    /// Interaction logic for CreateUserAccountView.xaml
+    /// </summary>
+    public partial class CreateUserAccountView : SingleViewBase
+    {
+        public CreateUserAccountView()
+        {
+            InitializeComponent();
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is CreateUserAccountViewModel vm)
+            {
+                vm.Password = ((PasswordBox)sender).Password;
+            }
+        }
+
+    }
+}
